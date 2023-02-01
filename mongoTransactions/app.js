ea5b1618d2af70faf7b016d09e7a4e2e7ee41ccc
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-01 17:43:27 -0300

    Add files via upload

--- mongoTransactions/app.js
+++ mongoTransactions/app.js
@@ -8,10 +8,10 @@ const dbname = "banko";
 const accounts = client.db(dbname).collection("accounts");
 const transfers = client.db(dbname).collection("transfers");
 
-// accounts information
-let account_id_sender = "MDB034232889";
+// accounts information sender: mauricio dananiel - receiver: bruxchil saez
+let account_id_sender = "MDB033221145";
 let account_id_receiver = "MDB0325759";
-const transaction_amount = 100;
+const transaction_amount = 500;
 
 // session
 const session = client.startSession();
@@ -21,10 +21,42 @@ const main = async () => {
     // aquí comienza proceso de transacción
 
     const transactionResults = await session.withTransaction(async () => {
-      //
+      // step 1: update the account sender balance
+      const updateSenderResults = await accounts.updateOne(
+        { account_id: account_id_sender },
+        { $inc: { balance: -transaction_amount } },
+        { session }
+      );
+
+      // step 2: update the account receiver balance
+      const updateReceiverResults = await transfers.updateOne(
+        { account_id: account_id_receiver },
+        { $inc: { balance: transaction_amount } },
+        { session }
+      );
+
+      // step 3: create a transfer document and insert it into the transfers collection
+      const transfer = {
+        transfer_id: "TR21872187",
+        amount: 100,
+        from_account: account_id_sender,
+        to_account: account_id_receiver,
+      };
+
+      const insertTransferResults = await transfers.insertOne(transfer, {
+        session,
+      });
+
+      // step 4 and 5: stand by
     });
+
+    if (transactionResults) {
+      console.log("Transaction Succeful");
+    } else {
+      console.log("The Transaction was Aborted");
+    }
   } catch (e) {
-    console.error(`Error de conexión, ${e}`);
+    console.error(`Transaction aborted, ${e}`);
   } finally {
     await client.close();
   }
