4d332e9fc1199ecf9f6a7c7c744a7cb7d2e93f01
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-07 21:28:14 -0300

    Add files via upload

--- NodeProjects/etudeProjects/etudeOneNode/src/index.js
+++ NodeProjects/etudeProjects/etudeOneNode/src/index.js
@@ -2,35 +2,43 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
-const session = require("express-sessions");
+const session = require("express-session");
 
 // Initializations
 const app = express();
+const sessionOptions = session({
+  secret: "este es el secreto",
+
+  // store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+
+// console.log(require("./routes/index"));
 
 // Settings (sección de configuración)
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
-app.engine(
-  ".hbs",
-  exphbs({
-    defaultLayout: "main",
-    layoutsDir: path.join(app.get("views"), "layouts"),
-    partialsDir: path.join(app.get("views"), "partials"),
-    extname: ".hbs",
-  })
-);
+
+// config view engine
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  partialsDir: path.join(app.get("views"), "partials"),
+  extname: ".hbs",
+});
+
+app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-app.use(
-  session({
-    secret: "mysecretapp",
-    resave: true,
-    saveUnitilialized: true,
-  })
-);
+app.use(sessionOptions);
 
 // Globals variables
 
