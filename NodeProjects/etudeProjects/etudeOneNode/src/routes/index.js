4ac2b0abce72c76a8d2e8cb755ef90098607fc07
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-08 21:51:35 -0300

    Add files via upload

--- NodeProjects/etudeProjects/etudeOneNode/src/routes/index.js
+++ NodeProjects/etudeProjects/etudeOneNode/src/routes/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/", (res, req) => {
+  res.send("index");
+});
+
+router.post("/about", (res, req) => {
+  res.send("about");
+});
+console.log(router);
 module.exports = router;
