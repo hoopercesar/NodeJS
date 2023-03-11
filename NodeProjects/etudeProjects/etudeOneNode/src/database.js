945425bfa83a78cf9a9e9a345c1522e8fd586356
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-11 09:47:42 -0300

    Add files via upload

---
+++ NodeProjects/etudeProjects/etudeOneNode/src/database.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require("mongodb");
+const uri = require("./atlas_uri");
+
+const client = new MongoClient(uri);
+const dbname = "nuevo";
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log(`Conexión a la DB ${dbname} con éxito`);
+
+    // Lista las bases de datos en el cluster
+    const dbs = await client.db().admin().listDatabases();
+    // console.table(dbs.databases);
+  } catch (err) {
+    console.error(`Error de conección a la DB, ${err}`);
+  }
+};
+
+const main = async () => {
+  try {
+    await connectToDatabase();
+  } catch (e) {
+    console.error(`Error de conexión, ${e}`);
+  } finally {
+    await client.close();
+  }
+};
+
+module.exports = main;
