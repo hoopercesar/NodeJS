8e510e0f6d1c6a3cd3891f0972bd330130de01e2
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-14 22:30:07 -0300

    Add files via upload

--- NodeProjects/etudeProjects/etudeOneNode/src/routes/notes.js
+++ NodeProjects/etudeProjects/etudeOneNode/src/routes/notes.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Note = require("../models/Note");
+
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
+
+router.post("/notes/new-note", (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+
+  if (!title) errors.push({ text: "Write a Title" });
+  if (!description) errors.push({ text: "Write a description" });
+
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    const note = new Note({ title, description });
+
+    console.log(note);
+  }
+});
 
 router.get("notes", (req, res) => {
   res.send("notes from database");
 });
-
-console.log(express);
 
 module.exports = router;
