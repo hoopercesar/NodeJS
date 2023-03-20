d4ac517774bcbdbb0ddfdb68c236d3f4d7788140
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-19 22:48:20 -0300

    Add files via upload

--- NodeProjects/etudeProjects/etudeOneNode/src/database.js
+++ NodeProjects/etudeProjects/etudeOneNode/src/database.js
@@ -1,8 +1,12 @@
 const { MongoClient } = require("mongodb");
 const uri = require("./atlas_uri");
+const note = require("./routes/notes");
 
 const client = new MongoClient(uri);
-const dbname = "nuevo";
+const dbname = "Chat";
+const collection_name = "conversaciones";
+
+const accountsCollection = client.db(dbname).collection(collection_name);
 
 const connectToDatabase = async () => {
   try {
@@ -11,15 +15,33 @@ const connectToDatabase = async () => {
 
     // Lista las bases de datos en el cluster
     const dbs = await client.db().admin().listDatabases();
-    // console.table(dbs.databases);
+    console.table(dbs.databases);
   } catch (err) {
     console.error(`Error de conección a la DB, ${err}`);
   }
 };
+const documentsToFind = { speak: 1 };
+
+let sampleAccounts = [
+  {
+    name: note.title,
+    mensaje: note.description,
+  },
+];
+// [
+//   {
+//     name: "pepe",
+//     mensaje: "hola mundo",
+//   },
+// ];
+// console.log(note);
 
 const main = async () => {
   try {
     await connectToDatabase();
+    let result = await accountsCollection.insertMany(sampleAccounts);
+    // let datos = await accountsCollection.find(documentsToFind);
+    console.log(result);
   } catch (e) {
     console.error(`Error de conexión, ${e}`);
   } finally {
